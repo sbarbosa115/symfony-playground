56e0441277d29a3c07ecede4876adcfad628ac57
Sergio Barbosa <s.barbosa@chargebacks911.com>
2021-02-07 19:52:16 -0500

    [added] Doctrine. [added] Migrations. [added] First entity.

--- src/Application/MessageHandler/ContactHandler.php
+++ src/Application/MessageHandler/ContactHandler.php
@@ -3,16 +3,31 @@ declare(strict_types=1);
 
 namespace App\Application\MessageHandler;
 
-
 use App\Application\Messages\ContactMessage;
+use App\Domain\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class ContactHandler implements MessageHandlerInterface
 {
 
+    public EntityManagerInterface $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
     public function __invoke(ContactMessage $message): void
     {
         // HERE STORE IT INTO THE DATABASE LOGIC
+        $payload = json_decode($message->getContact(), true, 512, JSON_THROW_ON_ERROR);
+        $user = (new User())
+            ->setEmail($payload['email']);
+
+        $this->manager->persist($user);
+        $this->manager->flush();
+
     }
 
 }
---
+++ src/Domain/Entity/User.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use App\Domain\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
